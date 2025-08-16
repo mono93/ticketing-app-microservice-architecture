5dd4e6d61e69d300e5922eae039459101595a12d
Monojit Saha <monojitsaha@Monojits-MacBook-Pro.local>
2025-08-16 14:05:08 +0530

    database management complete

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -13,18 +15,29 @@ const signupValidation = [
     .matches(/[a-z]/)
     .matches(/\d/)
     .matches(/[^A-Za-z0-9]/)
-    .not().matches(/\s/)
+    .not()
+    .matches(/\s/)
     .withMessage("Password must be valid"),
 ];
 
-const handleSignup = (req: Request, res: Response) => {
+const handleSignup = async (req: Request, res: Response) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     throw new RequestValidationError(errors.array());
   }
 
-  res.status(201).send({});
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw new BadRequestError("Email in use");
+  }
+
+  const user = User.build({ email, password });
+  await user.save();
+
+  res.status(201).send(user);
 };
 
 router.post("/api/users/signup", signupValidation, handleSignup);
