3e6a1ef0970306ed5837a3652ec524a0b8370d0d
Monojit Saha <monojitsaha@Monojits-MacBook-Pro.local>
2025-10-17 14:29:44 +0530

    expiration service - complete

---
+++ expiration/src/events/listener/order-created-listener.ts
@@ -0,0 +1,21 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@mstickets93/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - Date.now();
+    console.log(`waiting ${delay} miliseconds for processing`);
+    await expirationQueue.add({
+      orderId: data.id
+    }, {
+      delay
+    })
+
+    msg.ack()
+  }
+}
---
+++ expiration/src/events/publisher/expiration-complete-publisher.ts
@@ -0,0 +1,10 @@
+import {
+  Subjects,
+  Publisher,
+  ExpirationCompleteEvent,
+} from "@mstickets93/common";
+
+
+export class ExpirationCompletePublisher extends Publisher<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+}
